a5cbd020d354d4d9921cc9a322b267ae8148f34e
busenurhasdemir <busenurhasdemir@posta.mu.edu.tr>
2026-06-22 20:15:35 +0300

    idmanPrject is added

---
+++ IdmanProject/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Idman programına hoşgeldiniz!...");
+        String idmanlar = "Geçerli idmanlar...\n"+
+                          "Burpee\n"+
+                          "Pushup\n"+
+                          "Situp\n"+
+                          "Squat";
+        System.out.println(idmanlar);
+        System.out.println("Bir idman oluşturun...");
+        System.out.print("Burpee sayısı: ");
+        int burpee = scanner.nextInt();
+        System.out.print("Pushup sayısı: ");
+        int pushup = scanner.nextInt();
+        System.out.print("Situp sayısı: ");
+        int situp = scanner.nextInt();
+        System.out.print("Squat sayısı: ");
+        int squat = scanner.nextInt();
+        scanner.nextLine();
+
+        Idman idman = new Idman(burpee,pushup,situp,squat);
+        System.out.println("Idmanınız başlıyor...");
+
+        while (idman.idmanBittiMi()==false){
+            System.out.print("Hareket Türü (Burpee, Pushup, Situp, Squat): ");
+            String tur = scanner.nextLine();
+            System.out.print("Bu hareketten kaç tane yapacaksınız?: ");
+            int sayi = scanner.nextInt();
+            scanner.nextLine();
+            idman.hareketYap(tur,sayi);
+        }
+        System.out.println("İdmanınızı başarı ile tamamladınız...");
+
+
+
+    }
+}
